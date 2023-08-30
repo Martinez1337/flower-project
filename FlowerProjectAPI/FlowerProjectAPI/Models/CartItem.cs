bede89f322983c25744953fd847f80c31332b76d
Martinez1337 <90857594+Martinez1337@users.noreply.github.com>
2023-08-30 21:27:05 +0300

    Added new CartItem model

--- FlowerProjectAPI/FlowerProjectAPI/Models/CartItem.cs
+++ FlowerProjectAPI/FlowerProjectAPI/Models/CartItem.cs
@@ -2,12 +2,15 @@ namespace FlowerProjectAPI.Models;
 
 public class CartItem : Item
 {
+    public string CategoryName { get; set; }
+    
     public int Quantity { get; set; }
     
-    public CartItem(int id, string name, int categoryId, decimal price, int count, int quantity, 
+    public CartItem(int id, string name, int categoryId, string categoryName, decimal price, int count, int quantity, 
         string? description = null, string? image = null) 
         : base(id, name, categoryId, price, count, description, image)
     {
+        CategoryName = categoryName;
         Quantity = quantity;
     }
 }
